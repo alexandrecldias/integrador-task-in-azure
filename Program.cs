72d02088754cb0c5a5aaced29dcf9f1cc8885de9
Alexandre Dias <alexandrecldias@gmail.com>
2025-07-04 12:55:06 -0300

    ajustes ref. inclusao do campo assinatura, insteration, activit

--- Program.cs
+++ Program.cs
@@ -1,27 +1,39 @@
-﻿using System;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Security.Policy;
+﻿using System.Net.Http.Headers;
 using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
-using static System.Net.WebRequestMethods;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 class Program
 {
-    // CONFIGURAÇÕES
-    private static string origemUrl = Environment.GetEnvironmentVariable("ORIGEM_URL");
-    private static string destinoUrl = Environment.GetEnvironmentVariable("DESTINO_URL");
-    private static string projetoDestino = Environment.GetEnvironmentVariable("PROJETO_DESTINO");
-    private static int idWorkItemOrigem = int.Parse(Environment.GetEnvironmentVariable("ID_WORK_ITEM_ORIGEM"));
-    private static int idPbiDestino = int.Parse(Environment.GetEnvironmentVariable("ID_PBI_DESTINO"));
+    //// CONFIGURAÇÕES
+    //private static string origemUrl = Environment.GetEnvironmentVariable("ORIGEM_URL");
+    //private static string destinoUrl = Environment.GetEnvironmentVariable("DESTINO_URL");
+    //private static string projetoDestino = Environment.GetEnvironmentVariable("PROJETO_DESTINO");
+    //private static int idWorkItemOrigem = int.Parse(Environment.GetEnvironmentVariable("ID_WORK_ITEM_ORIGEM"));
+    //private static int idPbiDestino = int.Parse(Environment.GetEnvironmentVariable("ID_PBI_DESTINO"));
 
-    private static string patOrigem = Environment.GetEnvironmentVariable("PAT_ORIGEM");
-    private static string patDestino = Environment.GetEnvironmentVariable("PAT_DESTINO");
+    //private static string patOrigem = Environment.GetEnvironmentVariable("PAT_ORIGEM");
+    //private static string patDestino = Environment.GetEnvironmentVariable("PAT_DESTINO");
+    //private static string iterationPath = Environment.GetEnvironmentVariable("ITERATION_PATH");
+    //private static string atividade = Environment.GetEnvironmentVariable("ATIVIDADE_TASK");
+    //private static string responsavel = Environment.GetEnvironmentVariable("RESPONSAVEL_TASK");
+
 
     static async Task Main()
     {
+        DotNetEnv.Env.Load("C:\\GITHUB\\IntegradorTaskAzure\\.env"); // Carrega o .env automaticamente
+                              // Agora você pode acessar as variáveis com segurança
+        string origemUrl = Environment.GetEnvironmentVariable("ORIGEM_URL");
+        string destinoUrl = Environment.GetEnvironmentVariable("DESTINO_URL");
+        string projetoDestino = Environment.GetEnvironmentVariable("PROJETO_DESTINO");
+        int idWorkItemOrigem = int.Parse(Environment.GetEnvironmentVariable("ID_WORK_ITEM_ORIGEM"));
+        int idPbiDestino = int.Parse(Environment.GetEnvironmentVariable("ID_PBI_DESTINO"));
+        string patOrigem = Environment.GetEnvironmentVariable("PAT_ORIGEM");
+        string patDestino = Environment.GetEnvironmentVariable("PAT_DESTINO");
+        string iterationPath = Environment.GetEnvironmentVariable("ITERATION_PATH");
+        string atividade = Environment.GetEnvironmentVariable("ATIVIDADE_TASK");
+        string responsavel = Environment.GetEnvironmentVariable("RESPONSAVEL_TASK");
+
+
         var workItem = await ObterWorkItem(origemUrl, patOrigem, idWorkItemOrigem);
 
         // LEITURA DOS DADOS
@@ -33,13 +45,15 @@ class Program
         string prioridade = workItem["fields"]["Microsoft.VSTS.Common.Priority"]?.ToString();
 
 
+
         // Aqui você escolhe um dos valores válidos
         string tipoTask = "Atendimento ao cliente"; // <- Altere conforme desejado
 
         // Criação da nova task
         await CriarTaskDestino(destinoUrl, patDestino, projetoDestino,
-         titulo, descricao, horasEstimadas, horasRestantes, horasCompletadas,
-         prioridade, tipoTask, idPbiDestino);
+            titulo, descricao, horasEstimadas, horasRestantes, horasCompletadas,
+            prioridade, tipoTask, idPbiDestino,
+            iterationPath, atividade, responsavel, idWorkItemOrigem);
 
         Console.WriteLine("Task criada com sucesso!");
     }
@@ -68,7 +82,11 @@ class Program
         string horasCompletadas,
         string prioridade,
         string tipoTask,
-        int idPbi)
+        int idPbi,
+        string iterationPath,
+        string atividade,
+        string responsavel,
+        int idWorkItemOrigem)
     {
         using var client = new HttpClient();
         client.BaseAddress = new Uri(baseUrl);
@@ -77,12 +95,16 @@ class Program
 
         var json = new JArray
         {
-            new JObject { { "op", "add" }, { "path", "/fields/System.Title" }, { "value", titulo } },
+            new JObject { { "op", "add" }, { "path", "/fields/System.Title" }, { "value",$"{idWorkItemOrigem}-{titulo}"  } },
             new JObject { { "op", "add" }, { "path", "/fields/System.Description" }, { "value", descricao } },
          // ADIÇÃO NO PATCH JSON
             new JObject { { "op", "add" }, { "path", "/fields/Microsoft.VSTS.Scheduling.OriginalEstimate" }, { "value", horasEstimadas } },
             new JObject { { "op", "add" }, { "path", "/fields/Microsoft.VSTS.Scheduling.Effort" }, { "value", horasCompletadas } },
             new JObject { { "op", "add" }, { "path", "/fields/Custom.DateWork" }, { "value", DateTime.UtcNow.ToString("o") } },
+            new JObject { { "op", "add" }, { "path", "/fields/System.IterationPath" }, { "value", iterationPath } },
+            new JObject { { "op", "add" }, { "path", "/fields/Microsoft.VSTS.Common.Activity" }, { "value", atividade } },
+            new JObject { { "op", "add" }, { "path", "/fields/System.AssignedTo" }, { "value", responsavel } },
+
 
             new JObject { { "op", "add" }, { "path", "/fields/Microsoft.VSTS.Scheduling.CompletedWork" }, { "value", horasCompletadas } },
             new JObject { { "op", "add" }, { "path", "/fields/Microsoft.VSTS.Common.Priority" }, { "value", prioridade } },
